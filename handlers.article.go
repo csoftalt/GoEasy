4d190184e79826f9060a365d5b2756660d0fafda
Vickouma77 <Vickprestone20@gmail.com>
2025-06-02 12:08:44 +0300

    refactor(article): streamline rendering logic for index and article pages

--- handlers.article.go
+++ handlers.article.go
@@ -10,23 +10,34 @@ func showIndexPage(ctx *gin.Context) {
 
 	articles := getAllArticles()
 
-	ctx.HTML(http.StatusOK, "index.html", gin.H{
+	render(ctx, gin.H{
 		"title": "Home Page",
-		"payload": articles,
-	})
+		"payload": articles}, "index.html")
 }
 
 func getArticle(ctx *gin.Context) {
 	if articleID, err := strconv.Atoi(ctx.Param("article_id")); err == nil {
 		if article, err := getArticleByID(articleID); err == nil {
-			ctx.HTML(http.StatusOK, "article.html", gin.H{
+			render(ctx, gin.H{
 				"title":   article.Title,
 				"payload": article,
-			})
+			}, "article.html")
+			
 		} else {
 			ctx.AbortWithError(http.StatusNotFound, err)
 		}
 	} else {
 		ctx.AbortWithStatus(http.StatusNotFound)
+	}
+}
+
+func render(ctx *gin.Context, data gin.H, templateName string) {
+	switch ctx.Request.Header.Get("Accept") {
+	case "application/json":
+		ctx.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		ctx.XML(http.StatusOK, data["payload"])
+	default:
+		ctx.HTML(http.StatusOK, templateName, data)
 	}
 }
